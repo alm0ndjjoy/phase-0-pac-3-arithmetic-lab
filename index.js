c02f178ef384feeee020046e47c6d0de6ecc7b69
Joyce <JoyceHuang1025@gmail.com>
2021-09-01 12:26:54 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b){
+    return a+b
+}
+add();
+
+function subtract(a, b){
+    return a-b
+}
+subtract();
+
+function multiply(a, b){
+    return a*b
+}
+multiply();
+
+function divide(a, b){
+    return a/b
+}
+divide();
+
+function increment(n){
+    n++;
+    return(n)
+}
+increment(n);
+
+function decrement(n){
+    n--;
+    return(n)
+}
+decrement(n);
+
+function makeInt(string){
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string){
+    return parseFloat(string);
+}
